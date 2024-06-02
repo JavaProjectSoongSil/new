ae5d9930d0cc20c5b1f7bf27974bb80cd7a8ee58
kimyechan <tioon74@naver.com>
2024-06-02 21:49:35 +0900

    Chore: 카드설명란 수정

--- src/Card/impl/AttackCard.java
+++ src/Card/impl/AttackCard.java
@@ -9,13 +9,13 @@ import java.util.List;
 public class AttackCard extends Card {
     public AttackCard() {
         super(new HashMap<>() {{
-            put("damage", Arrays.asList(10, 1)); // 예: 10의 피해를 상대에게
+            put("attack", Arrays.asList(10, 1)); // 예: 10의 피해를 상대에게
         }}, "기본 공격 카드로 10의 피해를 상대방에게 줍니다.");
     }
 
     public AttackCard(int damage, int target) {
         super(new HashMap<>() {{
-            put("damage", Arrays.asList(damage, target));
+            put("attack", Arrays.asList(damage, target));
         }}, String.format("공격 카드로 %d의 피해를 상대방에게 줍니다.", damage, target));
     }
 }
--- src/Card/impl/DefenseCard.java
+++ src/Card/impl/DefenseCard.java
@@ -8,13 +8,13 @@ import java.util.HashMap;
 public class DefenseCard extends Card {
     public DefenseCard() {
         super(new HashMap<>() {{
-            put("defense", Arrays.asList(5, 0)); // 예: 5의 방어력을 나에게
-        }}, "기본 방어 카드로 5의 피해를 막아줍니다.");
+            put("defense", Arrays.asList(1, 0)); // 예: 5의 방어력을 나에게
+        }}, "기본 방어 카드로 영구적으로 방어력을 1 올려줍니다.");
     }
 
     public DefenseCard(int defense, int target) {
         super(new HashMap<>() {{
             put("defense", Arrays.asList(defense, target));
-        }}, String.format("공격 카드로 %d의 피해를 상대방에게 줍니다.", defense));
+        }}, String.format("방어 카드로 영구적으로 방어력을 %d 올려줍니다.", defense));
     }
 }
