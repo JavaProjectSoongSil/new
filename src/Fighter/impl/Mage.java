b02af8f1d6a67a4383687d75663310f385876fd4
kimyechan <tioon74@naver.com>
2024-06-02 23:57:59 +0900

    Refactor: Mage 기초 카드 수정

--- src/Fighter/impl/Mage.java
+++ src/Fighter/impl/Mage.java
@@ -11,22 +11,24 @@ import Fighter.Fighter;
 public class Mage extends Fighter {
     public Mage(String difficulty, int round) {
         super("Mage",
-                (int) Math.round(100 * (1 + ((double) round / 10))),
-                (int) Math.round(30 * (1 + ((double) round / 10))),
+                (int) Math.round(80 * (1 + ((double) round / 10))),
+                (int) Math.round(20 * (1 + ((double) round / 10))),
                 (int) Math.round(5 * (1 + ((double) round / 10))),
                 2);
         if (difficulty.equals("상")) {
             resource.put("HP", (int) Math.round(120 * (1 + ((double) round / 10)))); // HP를 높임
-            resource.put("attackPower", (int) Math.round(35 * (1 + ((double) round / 10)))); // 공격력을 높임
+            resource.put("attackPower", (int) Math.round(25 * (1 + ((double) round / 10)))); // 공격력을 높임
             resource.put("defensePower", (int) Math.round(7 * (1 + ((double) round / 10)))); // 방어력을 높임
         } else if (difficulty.equals("하")) {
             resource.put("HP", (int) Math.round(80 * (1 + ((double) round / 10)))); // HP를 낮춤
-            resource.put("attackPower", (int) Math.round(25 * (1 + ((double) round / 10)))); // 공격력을 낮춤
+            resource.put("attackPower", (int) Math.round(15 * (1 + ((double) round / 10)))); // 공격력을 낮춤
             resource.put("defensePower", (int) Math.round(3 * (1 + ((double) round / 10)))); // 방어력을 낮춤
         }
         // 카드 추가
-        cardSet.add(new AttackCard());
-        cardSet.add(new DefenseCard());
-        cardSet.add(new HealCard());
+        cardSet.add(new AttackCard(25,1));
+        cardSet.add(new AttackCard(30,1));
+        cardSet.add(new DefenseCard(3,0));
+        cardSet.add(new HealCard(25,0));
+        cardSet.add(new HealCard(30,0));
     }
 }
